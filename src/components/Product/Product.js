a56da766804d702891eee20136fe8d3cafc46d4c
florinpana01 <florinpana98@gmail.com>
2023-12-13 12:17:41 +0100

    initial backend connection achieved

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,33 @@
-// src/Product/Product.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Product() {
-  // Replace this with your product content
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('http://localhost:8001/api/products');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []); // Empty dependency array ensures useEffect runs only once (on mount)
+
   return (
-    <div className="product">
-      <h2>Sample Product</h2>
-      <p>Description: This is a sample product description.</p>
-      <p>Price: $19.99</p>
-      {/* Add more product information here */}
+    <div className="product-list">
+      <h2>Product List</h2>
+      {products.map((product) => (
+        <div key={product.id} className="product">
+          <h3>{product.title}</h3>
+          <p>Description: {product.description}</p>
+          {/* <p>Price: ${product.price.toFixed(2)}</p> */}
+          {/* Add more product information here */}
+        </div>
+      ))}
     </div>
   );
 }
